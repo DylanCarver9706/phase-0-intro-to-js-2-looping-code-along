71ec3e4827eec3be8448286e0bfab1dc4126b04b
Dylan <dylancarver14@gmail.com>
2022-06-17 23:08:28 -0500

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+// const names = ["Guadalupe", "Ollie", "Aki"];
+// const event = ["surprise"]
+function writeCards(namesArray, event) {
+    let thankYouMessages = []
+  for (let i = 0; i < namesArray.length; i++) {
+    thankYouMessages.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
+  }
+
+  return thankYouMessages;
+}
+
+let number = 10
+function countDown(number) {
+    let i = 0
+    while (i <= number) {
+        console.log(number--)
+        
+    }
+    return number
+}
+countDown(number)
